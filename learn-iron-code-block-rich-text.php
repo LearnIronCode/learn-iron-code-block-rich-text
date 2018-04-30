45b5dde5ec85b995008e14aea1b3962a38b8fc1e
Sal Ferrarello <sal@ironcodestudio.com>
2018-04-30 16:03:02 -0400

    Auto-generated files
    wp scaffold plugin learn-iron-code-block-rich-text --skip-tests --plugin_name="Learn Iron Code Block RichText" --plugin_author="Sal Ferrarello" --plugin_author_uri="https://salferrarello.com/" --plugin_description="Gutenberg Block Example" --plugin_uri="https://salferrarello.com/wordpress-custom-editor-block/"
    
    wp scaffold block iron-code-rich-text --title="Iron Code RichText" --plugin=learn-iron-code-block-rich-text

---
+++ learn-iron-code-block-rich-text.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Learn Iron Code Block RichText
+ * Plugin URI:      https://salferrarello.com/wordpress-custom-editor-block/
+ * Description:     Gutenberg Block Example
+ * Author:          Sal Ferrarello
+ * Author URI:      https://salferrarello.com/
+ * Text Domain:     learn-iron-code-block-rich-text
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Learn_Iron_Code_Block_Rich_Text
+ */
+
+// Your code starts here.
